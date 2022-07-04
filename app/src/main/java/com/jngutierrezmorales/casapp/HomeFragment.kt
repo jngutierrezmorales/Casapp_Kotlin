e7b7b2f5e5d3f402737879fe09343a7fe1ffa913
jngutierrezmorales <jngutierrezmorales@gmail.com>
2022-07-04 11:24:38 +0200

    prueba

---
+++ app/src/main/java/com/jngutierrezmorales/casapp/HomeFragment.kt
@@ -0,0 +1,21 @@
+package com.jngutierrezmorales.casapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
